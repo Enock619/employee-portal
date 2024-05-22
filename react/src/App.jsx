a849d34971d243f4939d5f88be32eace07c748f1
Enock Omweno <Eomweno@travelers.com>
2024-05-22 10:53:48 -0400

    Updated routing on App

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -2,17 +2,32 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
 
 // import components 
 import Employee from './components/Employee'
+import Home from './components/Home';
+import Login from './components/Login';
+import EmployeeSearch from './components/EmployeeSearch';
+import CareerChangePredictor from './components/CareerChangePredictor';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <>
-         <h1>Employee Directory</h1> 
-    </>
+    <Router>
+         <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/search' element={<EmployeeSearch />} />
+          <Route path='/career' element={<CareerChangePredictor />} />
+
+          </Routes> 
+    </Router>
   )
 }
 
--- react/src/components/EmployeeSearch.jsx
+++ react/src/components/EmployeeSearch.jsx
@@ -4,7 +4,7 @@ const EmployeeSearch = () => {
 
     return (
         <>
-
+        <h1>Search</h1>
         </>
     )
 }
