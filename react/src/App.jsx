61a7f7cbaed75cdbc5b6258f00bb7c83eb3cf08c
Max Klein <max@webage.com>
2024-05-22 11:21:29 -0400

    fixed data fetching

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -17,13 +17,31 @@ import EmployeeSearch from './components/EmployeeSearch';
 import CareerChangePredictor from './components/CareerChangePredictor';
 
 function App() {
-  
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+        try {
+            const response = await fetch(import.meta.env.VITE_EMPLOYEES_API_URL);
+            if (!response.ok) {
+                throw new Error('Data could not be fetched!');
+            }
+            const json_response = await response.json();
+            setData(json_response); // assign JSON response to the data variable.
+        } catch (error) {
+            console.error('Error fetching socks:', error);
+        }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <Router>
          <Routes>
           <Route exact path='/' element={<Home />} />
           <Route path='/login' element={<Login />} />
-          <Route path='/search' element={<EmployeeSearch />} />
+          <Route path='/search' element={<EmployeeSearch data={data}/>} />
           <Route path='/career' element={<CareerChangePredictor />} />
 
           </Routes> 
--- react/src/components/EmployeeSearch.jsx
+++ react/src/components/EmployeeSearch.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import Employee from "./Employee";
 
-const EmployeeSearch = () => {
+const EmployeeSearch = (props) => {
 
     return (
         <>
         <h1>Search</h1>
+        <div className="card-container" style={{ display: 'flex', flexWrap: 'wrap', gap: '20px' }}>
+            {
+                props.data.map((employee) => (
+                    <Employee key={employee._id} data={employee} />
+                ))
+            }
+        </div>
         </>
     )
 }
