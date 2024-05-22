bed5597e84caae769492f548ff36142e1a5f2308
Enock Omweno <Eomweno@travelers.com>
2024-05-22 11:23:21 -0400

    no changes

--- react/src/components/EmployeeSearch.jsx
+++ react/src/components/EmployeeSearch.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Employee from "./Employee";
 
 const EmployeeSearch = () => {
 
     return (
         <>
         <h1>Search</h1>
+        <Employee />
         </>
     )
 }
