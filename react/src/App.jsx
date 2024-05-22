9a06969b7599b88ff6438549cd85e97256cff44a
Mae <Mae@webage.com>
2024-05-22 11:25:06 -0400

    merging...

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Link
-} from "react-router-dom";
+import React from 'react';
 
-// import components 
-import Employee from './components/Employee'
-import Home from './components/Home';
-import Login from './components/Login';
-import EmployeeSearch from './components/EmployeeSearch';
-import CareerChangePredictor from './components/CareerChangePredictor';
+const Employee = ({ employees }) => {
+    return (
+        <div>
+            <h1>Employee Information</h1>
+            {employees.map(employee => (
+                <div key={employee._id} className="card card-background" style={{ flex: '1', minWidth: '300px', maxWidth: '45%' }}>
+                    <div className="card-body">
+                        <h5 className="card-title">Employee Details</h5>
+                        <div className="card-text">Name: {employee.name}</div>
+                        <div className="card-text">Phone: {employee.phone}</div>
+                        <div className="card-text">Job Role: {employee.job_role}</div>
+                        <div className="card-text">Work Location: {employee.work_location}</div>
+                        <div className="card-text">Salary: ${employee.salary}</div>
+                    </div>
+                    <div className="card-footer">
+                        <small className="text-muted">Employee ID: {employee.employee_id}</small>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
 
-function App() {
-  
-  return (
-    <Router>
-         <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/search' element={<EmployeeSearch />} />
-          <Route path='/career' element={<CareerChangePredictor />} />
-
-          </Routes> 
-    </Router>
-  )
-}
-
-export default App
+export default Employee;
